7f444c2662ec19c7f90595ac7fd7e47feb1a7d2f
mac-spec <lorven.riya.feb2007@gmail.com>
2026-09-11 19:18:26 +0530

    Show SIH capability status labels in governance centre

--- src/components/GovernanceCenter.tsx
+++ src/components/GovernanceCenter.tsx
@@ -1,9 +1,10 @@
 import { AuditCommandCenter } from './AuditCommandCenter';
+import { PrototypeStatus } from './PrototypeStatus';
 import { useEffect, useState } from 'react';
 import { fetchWorkOrders, type WorkOrder } from '@/services/auditService';
 
 export function GovernanceCenter() {
   const [orders, setOrders] = useState<WorkOrder[]>([]);
   useEffect(() => { void fetchWorkOrders().then(setOrders); }, []);
-  return <AuditCommandCenter workOrders={orders} />;
+  return <div className="space-y-4 sm:space-y-6"><PrototypeStatus /><AuditCommandCenter workOrders={orders} /></div>;
 }
